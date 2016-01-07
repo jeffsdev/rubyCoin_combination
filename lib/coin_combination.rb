781f896876dfc6651037c5a96993b34a49842ba3
Jeff Seymour and mike mahoney <mahonmr@me.com>
2016-01-07 10:11:36 -0800

    refactored code

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,16 +1,16 @@
 class Fixnum
   def coin_slide
     #coins = {25 => "quarter", 10 => "dime", 5 => "nickel", 1 => "penny" }
-    coins = [25, 10, 5, 1]
-    change = self
+    coin_values = [25, 10, 5, 1]
+    raw_change = self
     counter = 0
-    total_change = []
+    total_coins = []
     until counter.==(4)
-      temp_array = change.divmod(coins[counter])
-      total_change << temp_array[0]
-      change = temp_array[1]
-      counter = counter.+(1)
+      temp_array = raw_change.divmod(coin_values[counter])
+      total_coins << temp_array[0]
+      raw_change = temp_array[1]
+      counter += 1
     end
-    return total_change
+    return total_coins
   end
 end
