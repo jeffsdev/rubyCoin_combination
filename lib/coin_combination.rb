023ab32ebaa94e931c52c60a40d767ed58647a09
Jeff Seymour and mike mahoney <mahonmr@me.com>
2016-01-07 11:40:16 -0800

    add code to change output to words

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,6 +1,6 @@
 class Fixnum
+  require 'active_support/inflector'
   def coin_slide
-    #coins = {25 => "quarter", 10 => "dime", 5 => "nickel", 1 => "penny" }
     coin_values = [25, 10, 5, 1]
     raw_change = self
     counter = 0
@@ -11,6 +11,18 @@ class Fixnum
       raw_change = temp_array[1]
       counter += 1
     end
-    return total_coins
+    return coins_to_words(total_coins)
+  end
+
+  def coins_to_words(total_coins)
+    test = []
+    coins = {0 => "Quarter", 1 => "dime", 2 => "nickel", 3 => "penny" }
+    total_coins.each_with_index do |coin, index|
+      unless coin == 0
+        coin > 1 ? amount = coins.fetch(index).pluralize : amount = coins.fetch(index)
+        test << "#{coin} #{amount} "
+      end
+    end
+    return test.join
   end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -3,19 +3,25 @@ require('coin_combination')
 require('pry')
 
 describe('possible coin combinations') do
-  it('returns 2 quarters and 1 penny if change is 51 cents') do
-    expect(51.coin_slide()).to(eq([2,0,0,1]))
+  it('returns 2 quarters') do
+    expect(50.coin_slide()).to(eq('2 Quarters '))
+  end
+  it('returns 1 quarter') do
+    expect(25.coin_slide()).to(eq('1 Quarter '))
   end
   it('returns 3 quarters, 2 dimes, and 4 pennies if change is 99 cents') do
-    expect(99.coin_slide()).to(eq([3,2,0,4]))
+    expect(99.coin_slide()).to(eq('3 Quarters 2 dimes 4 pennies '))
   end
   it('returns 4 pennies if change is 4 cents') do
-    expect(4.coin_slide()).to(eq([0,0,0,4]))
+    expect(4.coin_slide()).to(eq('4 pennies '))
   end
   it('returns 1 dime if change is 10 cents') do
-    expect(10.coin_slide()).to(eq([0,1,0,0]))
+    expect(10.coin_slide()).to(eq('1 dime '))
   end
   it('returns 1 nickel and 2 pennies if change is 7 cents') do
-    expect(7.coin_slide()).to(eq([0,0,1,2]))
+    expect(7.coin_slide()).to(eq('1 nickel 2 pennies '))
+  end
+  it('returns 1 nickel and 2 pennies if change is 7 cents') do
+    expect(150.coin_slide()).to(eq('6 Quarters '))
   end
 end
